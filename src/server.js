4bb2e86d993943b6ef860f6508decbf2ca5886ed
Rohan Singh <rohan.singh.en@gmail.com>
2026-02-03 14:24:28 +0530

    feat: add graceful shutdown logic SIGTERM and SIGINT

--- src/server.js
+++ src/server.js
@@ -7,6 +7,24 @@ const server = app.listen(PORT, () => {
   logger.info(`Server running in ${env.nodeEnv} mode on http://localhost:${PORT}`);
 });
 
+/**
+ * Graceful shutdown handler
+ */
+async function shutdown(signal) {
+  logger.info(`Received ${signal}. Shutting down gracefully...`);
+
+  server.close(async () => {
+    try {
+      await prisma.$disconnect();
+      logger.info('Database connection closed');
+      process.exit(0);
+    } catch (err) {
+      logger.error('Error during shutdown', err);
+      process.exit(1);
+    }
+  });
+}
+
 process.on('unhandledRejection', (err) => {
   logger.error('Unhandled Rejection:', err);
   server.close(() => process.exit(1));
@@ -16,3 +34,9 @@ process.on('uncaughtException', (err) => {
   logger.error('Uncaught Exception:', err);
   process.exit(1);
 });
+
+/**
+ * Termination signals
+ */
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
